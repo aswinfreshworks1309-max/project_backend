3657f850ec76db7d215ba7f02acbf1ae0078b07c
aswinfreshworks1309-max <aswinfreshworks1309@gmail.com>
2026-01-25 20:09:28 +0530

    issue fixed

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -11,10 +11,6 @@ class UserBase(BaseModel):
 class UserCreate(UserBase):
     password: str
 
-class UserLogin(BaseModel):
-    email: str
-    password: str
-
 class User(UserBase):
     id: int
     
