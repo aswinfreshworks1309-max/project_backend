c043c9908d465c05500109120bd1fd40288f1d52
aswinfreshworks1309-max <aswinfreshworks1309@gmail.com>
2026-03-14 16:57:28 +0530

    middleware origin changed

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://locotranz.vercel.app"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
